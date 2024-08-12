f04ee78a38a8bc1b7c91a2db7e23fbff61fb6b39
Timothy <msiskazikani@gmail.com>
2024-08-12 10:49:35 +0200

    all interface models

--- bootstrapped/main/models.py
+++ bootstrapped/main/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+# START OF INTERFACE MODIFICATION MODELS (sections by sections)
+class Company(models.Model):
+    company_name=models.CharField(max_length=255)
+    motto=models.CharField(max_length=255)
+    about_txt=models.CharField(max_length=1024)
+
+class Hero(models.Model):
+    bg=models.ImageField(default="bg.png")
+
+
+class Pillars(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    fa_icon_name=models.CharField(max_length=105)
+    pillar_name=models.CharField(max_length=255)
+    pillar_txt=models.CharField(max_length=750)
+
+
+class Service(models.Model):
+    Company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    service=models.CharField(max_length=255)
+    img=models.ImageField(default="img.png")
+    description=models.CharField(max_length=1024)
+
+class Culture(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    fa_icon_name=models.CharField(max_length=105)
+    culture_name=models.CharField(max_length=255)
+    culture_txt=models.CharField(max_length=750)
+
+class Staff(models.Model):
+    fname=models.CharField(max_length=50)
+    lname=models.CharField(max_length=60)
+    dp=models.ImageField(default="staff.png")
+    position=models.CharField(max_length=75)
+    whatsapp=models.CharField(max_length=1024,blank=True)
+    instagram=models.CharField(max_length=1024,blank=True)
+    linkedIn=models.CharField(max_length=1024,blank=True)
+    phone=models.CharField(max_length=14)
+# END OF INTERFACE MODIFICATION MODELS

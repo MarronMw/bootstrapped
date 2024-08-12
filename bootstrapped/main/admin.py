dfe0c637a998abf4f2b8223eb353060974be8a00
Timothy <msiskazikani@gmail.com>
2024-08-12 15:10:02 +0200

    stuff ya bho

--- bootstrapped/main/admin.py
+++ bootstrapped/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Company,Hero,Pillars,Service,Culture,Staff
 # Register your models here.
+interface_models=[Company,Hero,Pillars,Service,Culture,Staff]
+admin.site.register(interface_models)
